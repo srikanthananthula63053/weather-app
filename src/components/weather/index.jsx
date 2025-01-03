91a6864d9f40172a5f47047227ea5ddb444081a3
srikanthananthula63053 <sreekanthananthula5@gmail.com>
2025-01-03 13:48:54 +0530

    adding css style

--- src/components/weather/index.jsx
+++ src/components/weather/index.jsx
@@ -1,51 +1,96 @@
 import { useEffect, useState } from "react";
 import Search from "../search";
 
-
 export default function Weather() {
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
 
-    const [search, setSearch] = useState("");
-    const [loading, setLoading] = useState(false);
-    const [weatherData, setWeatherData] = useState(null);
+  async function fetchWeatherData(param) {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=e34b4c51d8c2b7bf48d5217fe52ff79e`
+      );
 
-    async function FetchweatherData(param) {
-        setLoading(true)
-        try {
-            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${param}&appid=9c93d0e924ddc00810335b11cf24b7bc`)
-            const data = await response.json();
-            console.log(data)
-            if (data) {
-                setWeatherData(data);
-                setLoading(false)
-            }
-        } catch (e) {
-            setLoading(false)
-            console.log(e)
-
-        }
+      const data = await response.json();
+      if (data) {
+        setWeatherData(data);
+        setLoading(false);
+      }
+    } catch (e) {
+      setLoading(false);
+      console.log(e);
     }
+  }
 
-    useEffect(() => {
-        FetchweatherData("Hyderabad")
+  async function handleSearch() {
+    fetchWeatherData(search);
+  }
+function getCurrentDate(){
+    return new Date().toLocaleDateString('en-us',{
+        weekday:'long',
+        month:'long',
+        day:'numeric',
+        year:'numeric'
     })
+}
+  
 
-    async function handleSearch() {
-        FetchweatherData(search)
+  useEffect(() => {
+    fetchWeatherData("hyderabad");
+  }, []);
 
-    }
-    console.log(weatherData)
-    return (
+  console.log(weatherData);
+
+  return (
+    <div>
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
         <div>
-            <Search
-                search={search}
-                setSearch={setSearch}
-                handleSearch={handleSearch}
-            />
-            {
-                loading?<div> loading....</div>
-                :<div ></div>
-            }
-
+          <div className="city-name">
+            <h2>
+              {weatherData?.name}, <span>{weatherData?.sys?.country}</span>
+            </h2>
+          </div>
+          <div className="date">
+            <span>{getCurrentDate()}</span>
+          </div>
+          <div>{weatherData?.main?.temp}</div>
+          <p className="description">
+            {weatherData?weatherData.weather[0]&&weatherData.weather[0].description : ""}
+          </p>
+          <div className="weather-info">
+            <div>
+                <div>
+                    <p className="wind">{
+                        weatherData?.wind?.speed
+                        }
+                    </p>
+                    <p>wind speed</p>
+                </div>
+                
+            </div>
+            <div>
+                <div>
+                    <p className="humidity">{
+                        weatherData?.main?.humidity
+                        }
+                    </p>
+                    <p>Humidity</p>
+                </div>
+                
+            </div>
+          </div>
+         
         </div>
-    )
+      )}
+    </div>
+  );
 }
